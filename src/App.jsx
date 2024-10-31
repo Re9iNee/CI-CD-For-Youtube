96da64dc83f08b2c3003e784ab5f11cfadac4c1f
Re9iNee <attarzadeh76@gmail.com>
2024-10-31 13:41:40 +0330

    add vite react boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2>Implementing Continuous Integration and Continuous Deployment</h2>
+      <p>
+        A vite react and vitest boilerplate to demonstrate how to implement
+        CI/CD pipeline using GitHubActions
+      </p>
+    </>
+  );
+}
+
+export default App;
